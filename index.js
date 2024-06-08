41313a446adc0bbe1ad99f61a41c79d5381c0cee
Himanshu Gautam <165541817+himanshu20013@users.noreply.github.com>
2024-06-08 12:02:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,120 @@
+let num1 = 5;
+let num2 = 10;
+let sum = --num1 + ++num2 + num1--
+console.log(sum)
+
+// let checkin = true
+// let checkout = true
+
+// console.log(checkin == checkout);
+
+// let checkin = "world"
+// let checkout = "hello";
+
+//console.log(checkin == checkout);
+
+// let checkin = 10
+// let checkout = "10"
+
+// console.log(checkin === checkout);
+
+// let checkin = 10
+// let checkout = "10";
+// console.log(typeof checkin)
+// console.log(typeof checkout)
+// console.log(checkin == checkout);
+
+//let checkin = true
+//let chcekout = "10"
+//console.log(!checkin);
+
+//let checkin = "world"
+//let checkout = "hello";
+
+//console.log(checkin);
+
+//let checkin = "world"
+//let checkout = 10;
+
+//console.log(checkout);
+
+//let checkin = true
+//let checkout = true ;
+
+//console.log(checkin && checkout);
+
+//let checkin = true
+//let checkout = true ;
+
+//console.log(checkin && checkout);
+
+//let checkin = false
+//let checkout = false ;
+
+//console.log(!checkin && !checkout);
+
+//let checkin = false
+//let checkout = false ;
+
+//console.log(checkin || checkout);
+
+//let checkin = 10
+//let checkout = 20;
+//let result = (checkin == checkout)?"yes":"no"
+//console.log(result);
+
+//let checkin = 10
+//let checkout = 20;
+//let result = (checkin > checkout)?"yes":"no"
+//console.log(result);
+
+//let a = 10
+//let b = 20;
+//let c = 30
+//let result = (a > b && a > c)?"a":(b > c) ? "b" : "c";
+//console.log(result);
+
+//let a = 10
+//let b = 20;
+//let c = 30
+//let result = (a < b && a < c)?"a":(b < c) ? "b" : "c";
+//console.log(result);
+
+//let a = 10
+//let b = 20;
+//let c = 5
+//let result = (a < b && a > c)?"a":(b > c) ? "b" : "c";
+//console.log(result);
+
+//let age = 18
+//let result = (age<18)?"not vote":"vote";
+//console.log(result);
+
+//let num1 = 20;
+//let num2 = 30;
+
+//let result = (num1 > num2)? "yes": "no";
+//let result1 = (num1 > num2)? num1 : num2;
+//console.log(result);
+//console.log(result1);
+
+//let num1 = 0;
+//let num2 = 30;
+
+//let result = (num1 && num2)? "yes": "no";
+//console.log(result);
+
+//let num1 = 10;
+//let num2 = "";
+
+//let result = (num1 && num2)? "yes": "no";
+//console.log(result);
+
+//let num1 = 10;
+//let num2 = "";
+//if(num1){
+ // console.log("hello")
+//}
+
+//let result = (num1 && num2)? "yes": "no";
+//console.log(result);
